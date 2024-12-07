4a1bc58b3234a32fe954dd6e7b06acee82650d0e
hasan <hasanuzzaman.dev@gmail.com>
2024-12-07 08:17:38 +0600

    communication between 2 service using open-feing

--- employee-service/src/main/java/com/hasan/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/hasan/employeeservice/EmployeeServiceApplication.java
@@ -2,18 +2,14 @@ package com.hasan.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class EmployeeServiceApplication {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
