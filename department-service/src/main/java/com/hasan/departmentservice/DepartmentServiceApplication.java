7958e17563b4b813aab372b1f97f60c9c55a27f0
hasan <hasanuzzaman.dev@gmail.com>
2024-12-11 07:58:17 +0600

    add service-registry and add DepartmentServiceApplication to the eureka server

--- department-service/src/main/java/com/hasan/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/hasan/departmentservice/DepartmentServiceApplication.java
@@ -1,11 +1,13 @@
 package com.hasan.departmentservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@AutoConfiguration
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
