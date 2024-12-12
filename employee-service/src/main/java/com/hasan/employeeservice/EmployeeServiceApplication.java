685685145c92e6e6d6ed45aace3b8924a303b272
hasan <hasanuzzaman.dev@gmail.com>
2024-12-12 07:18:57 +0600

    add eureka client in EmployeeServiceApplication

--- employee-service/src/main/java/com/hasan/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/hasan/employeeservice/EmployeeServiceApplication.java
@@ -1,6 +1,7 @@
 package com.hasan.employeeservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@AutoConfiguration
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
