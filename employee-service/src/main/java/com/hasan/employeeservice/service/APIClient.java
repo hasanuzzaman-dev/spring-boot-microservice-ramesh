8b09becb38d6ce4f456cb2de95813b0b3517b9b8
hasan <hasanuzzaman.dev@gmail.com>
2024-12-16 10:23:01 +0600

    add api-gateway and configure application.yml

--- employee-service/src/main/java/com/hasan/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/hasan/employeeservice/service/APIClient.java
@@ -5,7 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "DEPARTMENT-SERVICE", url = "http://localhost:8080")
+
+/*
+
+ TODO: If there are multiple instance then it should be provide only name of the service
+       for load balancing. Eureka service provide load balancing internally
+*/
+
+//@FeignClient(name = "DEPARTMENT-SERVICE", url = "http://localhost:8080")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("/api/departments/{department-code}")
